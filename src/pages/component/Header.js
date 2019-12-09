4faac913d552e89047c986786437c9e3c60e891d
18703488177 <zq18703488177@163.com>
2019-12-09 21:12:43 +0800

    111

---
+++ src/pages/component/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import BScroll from '@better-scroll/core'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                 <header>
+                <div className="header-one">
+                    <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTS4yNSAxMi4zMzNWOWE4Ljc1IDguNzUgMCAwMTE3LjUgMHY1QTMuNzUgMy43NSAwIDAxMTQgMTcuNzVhLjQxNy40MTcgMCAwMTAtLjgzM2MuODE2IDAgMS41NTMtLjMzNSAyLjA4My0uODc1YTIuMDgzIDIuMDgzIDAgMDEtMi41LTIuMDQydi0xLjY2N2EyLjA4MyAyLjA4MyAwIDAxMy4zMzQtMS42NjZWOUE3LjkxNyA3LjkxNyAwIDAwMS4wODMgOXYxLjY2N2EyLjA4MyAyLjA4MyAwIDAxMy4zMzMgMS42NjdWMTRBMi4wODMgMi4wODMgMCAwMS4yNSAxNHYtMS42Njd6TTEuMDgzIDE0YTEuMjUgMS4yNSAwIDAwMi41IDB2LTEuNjY3YTEuMjUgMS4yNSAwIDAwLTIuNSAwVjE0em0xNS44MzQtMS42NjdhMS4yNSAxLjI1IDAgMDAtMi41IDBWMTRhMS4yNSAxLjI1IDAgMTAyLjUgMHYtMS42Njd6IiBmaWxsPSIjMzMzIi8+PC9zdmc+"/>
+                 <p>首页</p>
+                <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNLTEgMTloMjBWLTFILTF6Ii8+PHBhdGggZD0iTTE0IDcuMzMzYTYuNjY3IDYuNjY3IDAgMTEtMTMuMzM0IDAgNi42NjcgNi42NjcgMCAwMTEzLjMzNCAweiIgc3Ryb2tlPSIjMUExQTFBIiBzdHJva2Utd2lkdGg9Ii44MzMiLz48cGF0aCBkPSJNMTcuMzMzIDE3LjMzM2wtNS01IiBzdHJva2U9IiMxQTFBMUEiIHN0cm9rZS13aWR0aD0iLjgzMyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIi8+PC9nPjwvc3ZnPg=="></img>
+                </div>
+                <div className="wrapper" ref="wrapper2">
+                    <div className="header-two content" >
+                    <li><span>首页</span></li>
+                    <li><span>欢淘亲子</span></li>
+                    <li><span>超级旅行团</span></li>
+                    <li><span>和风东瀛</span></li>
+                    <li><span>梦幻暹罗</span></li>
+                    <li><span>诗画欧洲</span></li>
+                    <li><span>小众中东非</span></li>
+                    <li><span>美洲澳新</span></li>
+                    <li><span>祖国最美</span></li>
+                    </div>
+              </div>
+              </header>
+            </div>
+        )
+    }
+    componentDidMount(){
+         const  warpper=this.refs.wrapper2;
+         console.log(warpper);
+         
+         
+         this.scroll = new BScroll(warpper,{
+            scrollX : true
+          })
+          console.log(this.scroll);
+          
+    }
+}
+
